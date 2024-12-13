c78429690d0bf31aaf1ed5d5ea5c439ae14c7c31
Cuboid <fanjas112358@gmail.com>
2024-12-12 21:33:52 -0500

    fixing the calculator part
    removing obsolete gstack and releted ops/functions, renaming op_stack, adding annotations and small rewrites and stuff, and modified ev so it works on blocks of text now.

--- ev.py
+++ ev.py
@@ -1,36 +1,33 @@
 import op
+import err
 
 from collections import deque
 from decimal import Decimal, InvalidOperation, getcontext
 
 getcontext().prec = 15
-gstack: deque[Decimal] = deque()
 
 def format_stack(stack: deque[Decimal]) -> str:
-    formatted_strings: list[str] = list(map(lambda x: format(x, "f"), stack))
+    formatted_strings = [format(i, "f") for i in stack]
     joined = ", ".join(formatted_strings)
 
     return f"[{joined}]"
 
-def ev(s: str) -> None:
-    global gstack
-    tokens = s.split()
-    op_stack: deque[Decimal] = deque()
+def ev(s: str) -> deque[Decimal] | err.Error:
+    stack: deque[Decimal] = deque()
+    tokens = s.replace("\n", " ").split() # replace newlines with spaces so blocks of text work
 
     for token in tokens:
         try:
-            op_stack.append(Decimal(token))
+            stack.append(Decimal(token))
             continue
 
         except InvalidOperation:
             # (from decimal library)
             pass
 
-        error = op.handle(gstack, op_stack, token)
+        error = op.handle(stack, token)
 
         if error: # error!
-            return
+            return error
         
-    gstack.extend(op_stack)
-    print(f"op_stack: {format_stack(op_stack)}")
-    print(f"gstack: {format_stack(gstack)}")
+    return stack
