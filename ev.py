6ebc483ff1d55ee6d38eac1bb15099f49271a4ad
Cuboid <fanjas112358@gmail.com>
2024-12-20 21:43:39 -0500

    op.py is honestly unecessary
    me when file lolnger than 100 lines: (I am fr python programmer)
    anyways it's in ev.py now

--- ev.py
+++ ev.py
@@ -1,10 +1,71 @@
-import op
 import err
+import math
 
 from collections import deque
 from decimal import Decimal, InvalidOperation, getcontext
 
 getcontext().prec = 15
+def trypop(stack: deque[Decimal], token: str, index: int=-1) -> Decimal | err.Error:
+    try:
+        return stack.pop()
+
+    except IndexError:
+        return err.err("StackError", f"Not enough items on the stack at operator \"{token}\"")
+
+def handle(stack: deque[Decimal], token: str) -> None | err.Error:
+    # commands, i.e. zeronary operators
+    if token == "sd":
+        # stack delete
+        # deletes stack
+        stack.clear()
+
+    else:
+        # not zeronary operator
+        pop1: Decimal | err.Error = trypop(stack, token) # first pop for unary operators
+        if isinstance(pop1, err.Error):
+            return pop1
+
+        if token == "prec":
+            getcontext().prec = math.floor(pop1)
+
+        elif token == "sqrt":
+            stack.append(pop1.sqrt())
+
+        elif token == "log10":
+            stack.append(pop1.log10())
+
+        elif token == "ln":
+            stack.append(pop1.ln())
+
+        else:
+            # not unary operator
+            pop2: Decimal | err.Error = trypop(stack, token) # pop again for binary operators
+            if isinstance(pop2, err.Error):
+                return pop2
+
+            if token == "+":
+                stack.append(pop2 + pop1)
+
+            elif token == "-":
+                stack.append(pop2 - pop1)
+
+            elif token == "*":
+                stack.append(pop2 * pop1)
+
+            elif token == "/":
+                stack.append(pop2 / pop1)
+
+            elif token == "**":
+                stack.append(pop2 ** pop1)
+
+            elif token == "rt":
+                stack.append(pop2 ** (1 / pop1))
+
+            elif token == "log":
+                stack.append(pop1.log10() / pop2.log10())
+
+            else:
+                return err.err("OperatorError", f"Unknown Operator \"{token}\"")
 
 def format_stack(stack: deque[Decimal]) -> str:
     formatted_strings = [format(i, "f") for i in stack]
@@ -25,7 +86,7 @@ def ev(s: str) -> deque[Decimal] | err.Error:
             # (from decimal library)
             pass
 
-        error = op.handle(stack, token)
+        error = handle(stack, token)
 
         if error: # error!
             return error
