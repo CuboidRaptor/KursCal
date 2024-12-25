d3759dd1dd5e4cf2395cc41e244eb95a311c9d2b
Cuboid <fanjas112358@gmail.com>
2024-12-25 13:58:33 -0500

    errors are detected correctly now
    + massive refactor that makes this entire structure like. maintanable

--- ev.py
+++ ev.py
@@ -12,60 +12,45 @@ def trypop(stack: deque[Decimal], token: str, index: int=-1) -> Decimal | err.Er
     except IndexError:
         return err.err("StackError", f"Not enough items on the stack at operator \"{token}\"")
 
+# dictionary for operations
+# "op_name": (number_of_numbers_needed_from_stack, lambda stack, *args: ...)
+# operations like stack popping a specific index will have stack_nums=0
+def prec(stack, *args):
+    # man, python anon funcs suck....
+    getcontext().prec = math.floor(args[0])
+
+ops = {
+    "sd": (0, lambda stack: stack.clear()),
+    "prec": (1, prec),
+    "sqrt": (1, lambda stack, *args: stack.append(args[0].sqrt())),
+    "log10": (1, lambda stack, *args: stack.append(args[0].log10())),
+    "ln": (1, lambda stack, *args: stack.append(args[0].ln())),
+    "+": (2, lambda stack, *args: stack.append(args[1] + args[0])),
+    "-": (2, lambda stack, *args: stack.append(args[1] - args[0])),
+    "*": (2, lambda stack, *args: stack.append(args[1] * args[0])),
+    "/": (2, lambda stack, *args: stack.append(args[1] / args[0])),
+    "**": (2, lambda stack, *args: stack.append(args[1] ** args[0])),
+    "rt": (2, lambda stack, *args: stack.append(args[1] ** (1 / args[0]))),
+    "log": (2, lambda stack, *args: stack.append(args[0].log10() / args[1].log10())),
+}
+
 def handle(stack: deque[Decimal], token: str) -> None | err.Error:
-    # commands, i.e. zeronary operators
-    if token == "sd":
-        # stack delete
-        # deletes stack
-        stack.clear()
+    try:
+        pops: list[Decimal] = []
 
-    else:
-        # not zeronary operator
-        pop1: Decimal | err.Error = trypop(stack, token) # first pop for unary operators
-        if isinstance(pop1, err.Error):
-            return pop1
+        for i in range(0, ops[token][0]):
+            popped = trypop(stack, token)
 
-        if token == "prec":
-            getcontext().prec = math.floor(pop1)
-
-        elif token == "sqrt":
-            stack.append(pop1.sqrt())
-
-        elif token == "log10":
-            stack.append(pop1.log10())
-
-        elif token == "ln":
-            stack.append(pop1.ln())
-
-        else:
-            # not unary operator
-            pop2: Decimal | err.Error = trypop(stack, token) # pop again for binary operators
-            if isinstance(pop2, err.Error):
-                return pop2
-
-            if token == "+":
-                stack.append(pop2 + pop1)
-
-            elif token == "-":
-                stack.append(pop2 - pop1)
-
-            elif token == "*":
-                stack.append(pop2 * pop1)
-
-            elif token == "/":
-                stack.append(pop2 / pop1)
-
-            elif token == "**":
-                stack.append(pop2 ** pop1)
-
-            elif token == "rt":
-                stack.append(pop2 ** (1 / pop1))
-
-            elif token == "log":
-                stack.append(pop1.log10() / pop2.log10())
+            if isinstance(popped, err.Error):
+                return popped
 
             else:
-                return err.err("OperatorError", f"Unknown Operator \"{token}\"")
+                pops.append(popped)
+
+        ops[token][1](stack, *pops)
+
+    except KeyError:
+        return err.err("OperatorError", f"Unknown Operator \"{token}\"")
 
 def format_stack(stack: deque[Decimal]) -> str:
     formatted_strings = [format(i, "f") for i in stack]
