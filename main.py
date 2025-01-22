4fd4d6b02da4f8a637f5093e2a4de381f6fdd397
Cuboid <fanjas112358@gmail.com>
2025-01-21 23:03:27 -0500

    window
    text widget with no features in themed window
    yippee

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import window
 import editor
+import sys
 
 if __name__ == "__main__":
-    ...
+    sys.exit(window.app.exec())
--- window.py
+++ window.py
@@ -1,1 +1,32 @@
 import tomllib
+
+from PyQt6.QtGui import QFont
+from PyQt6.QtWidgets import QApplication, QWidget, QPlainTextEdit
+
+with open("./config.toml", "rb") as f:
+    CONFIG = tomllib.load(f)
+    COLORS = CONFIG["theme"]["colors"]
+    FONT = (CONFIG["theme"]["font"]["font"], CONFIG["theme"]["font"]["fontsize"])
+
+app = QApplication([])
+
+window = QWidget()
+window.setWindowTitle("KursCal")
+window.resize(864, 576)
+window.setStyleSheet(f"background-color: {COLORS['bg']};")
+window.setAutoFillBackground(True)
+
+font = QFont(*FONT)
+app.setFont(font, "QPlainTextEdit")
+
+vtext = QPlainTextEdit(window)
+vtext.resize(640, 500)
+vtext.setStyleSheet(f"""
+margin-left: 5px;
+margin-top: 5px;
+color: {COLORS['fg']};
+border: 2px solid {COLORS['fg']};
+""")
+
+
+window.show()
